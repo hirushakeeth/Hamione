c6dbb91d0b1f921afd8e8773dae6112e43dcd17a
hirushakeeth <77784129+hirushakeeth@users.noreply.github.com>
2021-02-06 15:34:12 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -63,7 +63,7 @@ My master admin is [Hirusha](t.me/matheeshaofficial)
 HELP_STRINGS = """
 Hey there! My name is *{}*.
 I'm lovely wizard 🧙‍♀ & I help admins to manage their groups! Have a look at the following for an idea of some of \
-"""
+[img](https://telegra.ph/file/0a9c3ecc4eec6738a001d.jpg)
 
 *Main* commands available:
  • /help: PM's you this message.
