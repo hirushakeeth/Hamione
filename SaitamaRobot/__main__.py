1b8624e408c9cfd2d84c2d3f7f6f4fd8364e76b3
hirushakeeth <77784129+hirushakeeth@users.noreply.github.com>
2021-02-07 12:01:45 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -80,7 +80,7 @@ And the following
     dispatcher.bot.first_name, ""
     if not ALLOW_EXCL else "\nAll commands can either be used with / or !.\n")
 
-SAITAMA_IMG = "https://telegra.ph/file/c607626b80bd0fc347045.jpg"
+SAITAMA_IMG = "https://telegra.ph/file/0c681ec5e19422ef3cfcb.jpg"
 
 DONATE_STRING =  """Heya, glad to hear you want to donate!
 hamione is hosted on one of Heroku's Servers and doesn't require any donations as of now but \
